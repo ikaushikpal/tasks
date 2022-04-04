80acfc14f4768f832e3190ffef5aa4d960710c3c
SupriyoDam <67065203+SupriyoDam@users.noreply.github.com>
2022-04-04 19:52:08 +0530

    first merge

--- src/db/database.py
+++ src/db/database.py
@@ -3,18 +3,14 @@ from typing import List, Optional
 import datetime
 from model import Task
 from pathlib import Path
-<<<<<<< HEAD
-from exceptions.exception import *
-=======
 try:
     from exceptions import exception
 except ModuleNotFoundError:
-    import importlib.util       
+    import importlib.util
     spec = importlib.util.spec_from_file_location(
     "exception", Path(__file__).parent.parent / "exceptions/exception.py")
-    exception = importlib.util.module_from_spec(spec)       
+    exception = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(exception)
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
 
 
 class DataBaseHandler():
@@ -32,20 +28,12 @@ class DataBaseHandler():
         try:
             self.database_path.parent.mkdir(exist_ok=True)
         except OSError:
-<<<<<<< HEAD
-            raise DirectoryError("data")
-=======
             raise exception.DirectoryError("data")
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
 
         try:
             self.database_path.touch(exist_ok=True)
         except OSError:
-<<<<<<< HEAD
-            return DataBaseCreationError()
-=======
             raise exception.DataBaseCreationError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
 
     def connect(self):
         try:
@@ -54,14 +42,11 @@ class DataBaseHandler():
             self.connection_status = True
         except Exception as e:
             raise ConnectionError()
-    
+
     def create_table(self):
         try:
-<<<<<<< HEAD
-=======
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
             with self.conn:
                 self.cursor.execute('''CREATE TABLE IF NOT EXISTS tasks
                                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -74,38 +59,26 @@ class DataBaseHandler():
                                     desc TEXT)''')
                 self.conn.commit()
         except Exception as e:
-<<<<<<< HEAD
-            raise DataBaseWriteError(str(e))
-    
-    def add_task(self, task: Task):
-        try:
-=======
             raise exception.DataBaseWriteError(str(e))
     
     def add_task(self, task: Task):
         try:
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
             with self.conn:
                 self.cursor.execute('INSERT INTO tasks(title, date_added, status, category, priority, date_completed, desc) VALUES (:title, :date_added, :status, :category, :priority, :date_completed, :desc)',
                 {'title': task.title, 'date_added':task.date_added,'status': task.status, 
                 'category': task.category, 'priority': task.priority, 'date_completed': task.date_completed, 'desc': task.desc})
             self.conn.commit()
-        except Exception as e:
-<<<<<<< HEAD
-            raise DataBaseWriteError(str(e))
 
-    def get_all_tasks(self) -> List[Task]:
-        try:
-=======
+        except Exception as e:
             raise exception.DataBaseWriteError(str(e))
 
     def get_all_tasks(self) -> List[Task]:
         try:
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
+
             with self.conn:
                 self.cursor.execute('SELECT * FROM tasks')
                 results = self.cursor.fetchall()
@@ -114,17 +87,11 @@ class DataBaseHandler():
                     tasks.append(Task(*result))
                 return tasks
         except Exception as e:
-<<<<<<< HEAD
-            raise DataBaseReadError(str(e))
-    
-    def get_tasks_by_cond(self, cond, sort=[]) -> List[Task]:
-=======
             raise exception.DataBaseReadError(str(e))
     
     def get_tasks_by_cond(self, cond, sort=[]) -> List[Task]:
         if not self.connection_status:
             raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
         cond_msg = ''
 
         for key, value in cond.items():
@@ -144,19 +111,12 @@ class DataBaseHandler():
                     tasks.append(Task(*result))
                 return tasks
         except Exception as e:
-<<<<<<< HEAD
-            raise DataBaseReadError(str(e))
-            
-    def get_tasks_by_date(self, date_fmt: str, date_column='date_added') -> List[Task]:
-        try:
-=======
             raise exception.DataBaseReadError(str(e))
             
     def get_tasks_by_date(self, date_fmt: str, date_column='date_added') -> List[Task]:
         try:
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
             with self.conn:
                 self.cursor.execute(f'SELECT * FROM tasks WHERE date({date_column}) LIKE :day', {'day': date_fmt})
                 results = self.cursor.fetchall()
@@ -166,29 +126,16 @@ class DataBaseHandler():
                 return tasks
 
         except Exception as e:
-<<<<<<< HEAD
-            raise DataBaseReadError(str(e))
-
-    def remove_task(self, id:int):
-        try:
-=======
             raise exception.DataBaseReadError(str(e))
 
     def remove_task(self, id:int):
         try:
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
             with self.conn:
                 self.cursor.execute('DELETE FROM tasks WHERE id = :id', {'id': id})
                 self.conn.commit()
         except Exception as e:
-<<<<<<< HEAD
-            raise IDNotFoundError(id)
-
-    def undone_task(self, id:int):
-        try:
-=======
             raise exception.IDNotFoundError(id)
 
     def remove_all_tasks(self, id:int):
@@ -205,35 +152,23 @@ class DataBaseHandler():
         try:
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
             with self.conn:
                 self.cursor.execute('UPDATE tasks SET status = 0 AND date_completed=NULL WHERE id = :id', {'id': id})
                 self.conn.commit()
         except Exception as e:
-<<<<<<< HEAD
-            raise IDNotFoundError(id)
-    
-    def done_task(self, id:int):
-        try:
-=======
             raise exception.IDNotFoundError(id)
     
     def done_task(self, id:int):
         try:
             if not self.connection_status:
                 raise exception.ConnectionError()
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
             with self.conn:
                 self.cursor.execute('UPDATE tasks SET status = 1 AND date_completed=:date_completed WHERE id = :id', {'id': id, 'date_completed': datetime.datetime.now().isoformat()})
                 self.conn.commit()
         except Exception as e:
-<<<<<<< HEAD
-            raise IDNotFoundError(id)
-=======
             raise exception.IDNotFoundError(id)
 
 
 if __name__ == '__main__':
     db = DataBaseHandler(Path(__file__).parent.parent.parent / 'data' / 'tasks.db')
     print(db.get_all_tasks())
->>>>>>> ff9cca984c394db3f4713da8a0a192526c32fe14
